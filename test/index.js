a1062ddda3de6bbbcd7f65f53b285b70e6231620
Adam Gotlib <gotlib.adam+dev@gmail.com>
2017-09-22 19:57:23 +0200

    More extensive testing

--- test/index.js
+++ test/index.js
@@ -60,12 +60,14 @@ test('toMs() test', (t) => {
   t.equal(toMs('02:00'), 120000)
   t.equal(toMs('02:00', 'hh:mm'), 7200000)
   t.equal(toMs('-04:35', 'hh:mm'), -16500000)
+  t.equal(toMs('00:00:07.10845', 'hh:mm'), 7108)
 
   // Input validation
   t.throws(() => toMs('13:05:02:11'))
   t.throws(() => toMs('153'))
   t.throws(() => toMs(null))
   t.throws(() => toMs('00:62'))
+  t.throws(() => toS('01:30', 'mm:hh:ss'))
 
   t.end()
 })
@@ -80,30 +82,68 @@ test('toS() test', (t) => {
   t.equal(toS('02:00'), 120)
   t.equal(toS('02:00', 'hh:mm'), 7200)
   t.equal(toS('-04:35', 'hh:mm'), -16500)
+  t.equal(toS('00:00:07.10845', 'hh:mm'), 7)
 
   // Input validation
   t.throws(() => toS('13:05:02:11'))
   t.throws(() => toS('153'))
   t.throws(() => toS(null))
   t.throws(() => toS('00:62'))
+  t.throws(() => toS('01:30', 'mm:hh:ss'))
 
   t.end()
 })
 
 test('symmetrical conversion test', (t) => {
-  // fromMs() and toMs() for all formats
-  t.equal(toMs(fromMs(900000, 'mm:ss'), 'mm:ss'), 900000)
-  t.equal(toMs(fromMs(915040, 'mm:ss.sss'), 'mm:ss.sss'), 915040)
-  t.equal(toMs(fromMs(4500000, 'hh:mm'), 'hh:mm'), 4500000)
-  t.equal(toMs(fromMs(4515000, 'hh:mm:ss'), 'hh:mm:ss'), 4515000)
-  t.equal(toMs(fromMs(4515690, 'hh:mm:ss.sss'), 'hh:mm:ss.sss'), 4515690)
+  /*
+   * fromMs() and toMs() for all formats
+   */
 
-  // fromS() and toMs() for all formats
-  t.equal(toS(fromS(900, 'mm:ss'), 'mm:ss'), 900)
-  t.equal(toS(fromS(921, 'mm:ss.sss'), 'mm:ss.sss'), 921)
-  t.equal(toS(fromS(4500, 'hh:mm'), 'hh:mm'), 4500)
-  t.equal(toS(fromS(4515, 'hh:mm:ss'), 'hh:mm:ss'), 4515)
-  t.equal(toS(fromS(4515, 'hh:mm:ss.sss'), 'hh:mm:ss.sss'), 4515)
+  // 90000ms = 1m 30s
+  t.equal(toMs(fromMs(90000, 'mm:ss'), 'mm:ss'), 90000)
+  t.equal(toMs(fromMs(90000, 'mm:ss.sss'), 'mm:ss.sss'), 90000)
+  t.equal(toMs(fromMs(90000, 'hh:mm'), 'hh:mm'), 90000)
+  t.equal(toMs(fromMs(90000, 'hh:mm:ss'), 'hh:mm:ss'), 90000)
+  t.equal(toMs(fromMs(90000, 'hh:mm:ss.sss'), 'hh:mm:ss.sss'), 90000)
+
+  // 7517245ms = 2h 5m 17.245s
+  t.equal(toMs(fromMs(7517245, 'mm:ss'), 'mm:ss'), 7517245)
+  t.equal(toMs(fromMs(7517245, 'mm:ss.sss'), 'mm:ss.sss'), 7517245)
+  t.equal(toMs(fromMs(7517245, 'hh:mm'), 'hh:mm'), 7517245)
+  t.equal(toMs(fromMs(7517245, 'hh:mm:ss'), 'hh:mm:ss'), 7517245)
+  t.equal(toMs(fromMs(7517245, 'hh:mm:ss.sss'), 'hh:mm:ss.sss'), 7517245)
+
+  // -10800000ms = -3h
+  t.equal(toMs(fromMs(-10800000, 'mm:ss'), 'mm:ss'), -10800000)
+  t.equal(toMs(fromMs(-10800000, 'mm:ss.sss'), 'mm:ss.sss'), -10800000)
+  t.equal(toMs(fromMs(-10800000, 'hh:mm'), 'hh:mm'), -10800000)
+  t.equal(toMs(fromMs(-10800000, 'hh:mm:ss'), 'hh:mm:ss'), -10800000)
+  t.equal(toMs(fromMs(-10800000, 'hh:mm:ss.sss'), 'hh:mm:ss.sss'), -10800000)
+
+  /*
+   * fromS() and toMs() for all formats
+   */
+
+  // 930s = 15m 30s
+  t.equal(toS(fromS(930, 'mm:ss'), 'mm:ss'), 930)
+  t.equal(toS(fromS(930, 'mm:ss.sss'), 'mm:ss.sss'), 930)
+  t.equal(toS(fromS(930, 'hh:mm'), 'hh:mm'), 930)
+  t.equal(toS(fromS(930, 'hh:mm:ss'), 'hh:mm:ss'), 930)
+  t.equal(toS(fromS(930, 'hh:mm:ss.sss'), 'hh:mm:ss.sss'), 930)
+
+  // 4850s = 1h 20m 50s
+  t.equal(toS(fromS(4850, 'mm:ss'), 'mm:ss'), 4850)
+  t.equal(toS(fromS(4850, 'mm:ss.sss'), 'mm:ss.sss'), 4850)
+  t.equal(toS(fromS(4850, 'hh:mm'), 'hh:mm'), 4850)
+  t.equal(toS(fromS(4850, 'hh:mm:ss'), 'hh:mm:ss'), 4850)
+  t.equal(toS(fromS(4850, 'hh:mm:ss.sss'), 'hh:mm:ss.sss'), 4850)
+
+  // -300s = -5m
+  t.equal(toS(fromS(-300, 'mm:ss'), 'mm:ss'), -300)
+  t.equal(toS(fromS(-300, 'mm:ss.sss'), 'mm:ss.sss'), -300)
+  t.equal(toS(fromS(-300, 'hh:mm'), 'hh:mm'), -300)
+  t.equal(toS(fromS(-300, 'hh:mm:ss'), 'hh:mm:ss'), -300)
+  t.equal(toS(fromS(-300, 'hh:mm:ss.sss'), 'hh:mm:ss.sss'), -300)
 
   t.end()
 })
